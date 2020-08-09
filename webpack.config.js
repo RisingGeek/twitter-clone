0ad2d16a38782a0872f732688c2d45803685d8a5
bhavay <bhavaypuri15@gmail.com>
2020-08-09 12:10:32 +0530

    Fix production error and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,53 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const { DefinePlugin } = require('webpack');
-const path = require('path');
-const config = require('dotenv').config();
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { DefinePlugin } = require("webpack");
+const path = require("path");
+const dotenv = require("dotenv");
 
-module.exports = {
+module.exports = (env, argv) => {
+  return {
     context: __dirname,
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        publicPath: '/',
+      path: path.resolve(__dirname, "dist"),
+      filename: "main.js",
+      publicPath: "/",
     },
     devServer: {
-        historyApiFallback: true,
-        port: 3000,
-        open: true
+      historyApiFallback: true,
+      port: 3000,
+      open: true,
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|j?g|svg|gif)?$/,
-                use: 'file-loader'
-            }
-        ]
+      rules: [
+        {
+          test: /\.js$/,
+          use: "babel-loader",
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|j?g|svg|gif)?$/,
+          use: "file-loader",
+        },
+      ],
     },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: path.resolve(__dirname, 'public/index.html'),
-            filename: 'index.html'
-        }),
-        new DefinePlugin({
-            'process.env': JSON.stringify(config.parsed)
-        })
-    ]
+      new HtmlWebPackPlugin({
+        template: path.resolve(__dirname, "public/index.html"),
+        filename: "index.html",
+      }),
+      new DefinePlugin({
+        "process.env":
+          argv.mode === "development"
+            ? JSON.stringify(
+                dotenv.config({
+                  path: path.resolve(__dirname, "./.env.development"),
+                }).parsed
+              )
+            : JSON.stringify(dotenv.config().parsed),
+      }),
+    ],
+  };
 };
